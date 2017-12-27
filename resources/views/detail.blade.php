9668e459b6997e28e9d135aae8631ba54c868e45
deki <dekiakbar@linuxmail.org>
2017-12-28 01:08:30 +0700

    akhirnya bisa juga layout

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -4,87 +4,80 @@
 @section('isi')
 <div class="ui container segment">
 	<div class="ui container">
-		<h3 class="ui center aligned header">Info Geolokasi</h3>
-		<div class="ui four column doubling stackable grid">
+		<h2 class="ui center aligned header">Informasi</h2>
+		<div class="ui three column doubling stackable grid">
 			<div class="column">
-			    <div class="ui segment">
-			    	<div class="ui relaxed divided list">
-					    <div class="item">
-					        <i class="large flag middle aligned icon blue"></i>
-					        <div class="content">
-					            <a class="header">Negara :</a>
-					            <div class="description">{{ $lokasi->negara }}</div>
-					        </div>
-					    </div>
+				<h3 class="ui center aligned header">Informasi Lokasi</h3>
+				<div class ="ui segment">
+					<div class ="ui relaxed divided list">
+						<div class ="item">
+							<i class   ="large flag middle aligned icon blue"></i>
+							<div class ="content">
+								<a class   ="header">Negara :</a>
+								<div class ="description">{{ $lokasi->negara }}</div>
+							</div>
+						</div>
 					</div>
-			    </div>
-			</div>
-			<div class="column">
-			    <div class="ui segment">
-			    	<div class="ui relaxed divided list">
-					    <div class="item">
-					        <i class="large address card outline middle aligned icon blue"></i>
-					        <div class="content">
-					            <a class="header">Provinsi :</a>
-					            <div class="description">{{ $lokasi->provinsi }}</div>
-					        </div>
-					    </div>
+				</div>
+				<div class ="ui segment">
+					<div class ="ui relaxed divided list">
+						<div class ="item">
+							<i class   ="large address card outline middle aligned icon blue"></i>
+							<div class ="content">
+								<a class   ="header">Provinsi :</a>
+								<div class ="description">{{ $lokasi->provinsi }}</div>
+							</div>
+						</div>
 					</div>
-			    </div>
-			</div>
-			<div class="column">
-			    <div class="ui segment">
-			    	<div class="ui relaxed divided list">
-					    <div class="item">
-					        <i class="large address card outline middle aligned icon blue"></i>
-					        <div class="content">
-					            <a class="header">Kota :</a>
-					            <div class="description">{{ $lokasi->kota }}</div>
-					        </div>
-					    </div>
+				</div>
+				<div class ="ui segment">
+					<div class ="ui relaxed divided list">
+						<div class ="item">
+							<i class   ="large address card outline middle aligned icon blue"></i>
+							<div class ="content">
+								<a class   ="header">Kota :</a>
+								<div class ="description">{{ $lokasi->kota }}</div>
+							</div>
+						</div>
 					</div>
-			    </div>
-			</div>
-			<div class="column">
-			    <div class="ui segment">
-			    	<div class="ui relaxed divided list">
-					    <div class="item">
-					        <i class="large address card outline middle aligned icon blue"></i>
-					        <div class="content">
-					            <a class="header">Kecamatan :</a>
-					            <div class="description">{{ $lokasi->kecamatan }}</div>
-					        </div>
-					    </div>
+				</div>
+				<div class ="ui segment">
+					<div class ="ui relaxed divided list">
+						<div class ="item">
+							<i class   ="large address card outline middle aligned icon blue"></i>
+							<div class ="content">
+								<a class   ="header">Kecamatan :</a>
+								<div class ="description">{{ $lokasi->kecamatan }}</div>
+							</div>
+						</div>
 					</div>
-			    </div>
-			</div>
-			<div class="column">
-			    <div class="ui segment">
-			    	<div class="ui relaxed divided list">
-					    <div class="item">
-					        <i class="large address card outline middle aligned icon blue"></i>
-					        <div class="content">
-					            <a class="header">Kelurahan :</a>
-					            <div class="description">{{ $lokasi->kelurahan }}</div>
-					        </div>
-					    </div>
+				</div>
+				<div class ="ui segment">
+					<div class ="ui relaxed divided list">
+						<div class ="item">
+							<i class   ="large address card outline middle aligned icon blue"></i>
+							<div class ="content">
+								<a class   ="header">Kelurahan :</a>
+								<div class ="description">{{ $lokasi->kelurahan }}</div>
+							</div>
+						</div>
 					</div>
-			    </div>
-			</div>
-			<div class="column">
-			    <div class="ui segment">
-			    	<div class="ui relaxed divided list">
-					    <div class="item">
-					        <i class="large address card middle aligned icon blue"></i>
-					        <div class="content">
-					            <a class="header">Alamat :</a>
-					            <div class="description">{{ $lokasi->alamat }}</div>
-					        </div>
-					    </div>
+				</div>
+				<div class ="ui segment">
+					<div class ="ui relaxed divided list">
+						<div class ="item">
+							<i class   ="large address card middle aligned icon blue"></i>
+							<div class ="content">
+								<a class   ="header">Alamat :</a>
+								<div class ="description">{{ $lokasi->alamat }}</div>
+							</div>
+						</div>
 					</div>
-			    </div>
+				</div>
 			</div>
+
 			<div class="column">
+				<h3 class="ui center aligned header">Informasi Geografis</h3>
 			    <div class="ui segment">
 			    	<div class="ui relaxed divided list">
 					    <div class="item">
@@ -96,8 +89,6 @@
 					    </div>
 					</div>
 			    </div>
-			</div>
-			<div class="column">
 			    <div class="ui segment">
 			    	<div class="ui relaxed divided list">
 					    <div class="item">
@@ -109,8 +100,6 @@
 					    </div>
 					</div>
 			    </div>
-			</div>
-			<div class="column">
 			    <div class="ui segment">
 			    	<div class="ui relaxed divided list">
 					    <div class="item">
@@ -122,8 +111,6 @@
 					    </div>
 					</div>
 			    </div>
-			</div>
-			<div class="column">
 			    <div class="ui segment">
 			    	<div class="ui relaxed divided list">
 					    <div class="item">
@@ -135,8 +122,6 @@
 					    </div>
 					</div>
 			    </div>
-			</div>
-			<div class="column">
 			    <div class="ui segment">
 			    	<div class="ui relaxed divided list">
 					    <div class="item">
@@ -148,8 +133,76 @@
 					    </div>
 					</div>
 			    </div>
+			    <div class="ui segment">
+			    	<div class="ui relaxed divided list">
+					    <div class="item">
+					        <i class="large cloud middle aligned icon blue"></i>
+					        <div class="content">
+					            <a class="header">Cuaca :</a>
+					            <div class="description">{{ $kondisi->cuaca }}</div>
+					        </div>
+					    </div>
+					</div>
+			    </div>
 			</div>
+
 			<div class="column">
+				<h3 class="ui center aligned header">Informasi Tanah</h3>
+			    <div class="ui segment">
+			    	<div class="ui relaxed divided list">
+					    <div class="item">
+					        <i class="large thermometer half middle aligned icon blue"></i>
+					        <div class="content">
+					            <a class="header">Suhu :</a>
+					            <div class="description">{{ $kondisi->suhu }}</div>
+					        </div>
+					    </div>
+					</div>
+			    </div>
+			    <div class="ui segment">
+			    	<div class="ui relaxed divided list">
+					    <div class="item">
+					        <i class="large percent middle aligned icon blue"></i>
+					        <div class="content">
+					            <a class="header">Kelembaban :</a>
+					            <div class="description">{{ $kondisi->kelembaban }}</div>
+					        </div>
+					    </div>
+					</div>
+			    </div>
+			    <div class="ui segment">
+			    	<div class="ui relaxed divided list">
+					    <div class="item">
+					        <i class="large random middle aligned icon blue"></i>
+					        <div class="content">
+					            <a class="header">Arah Angin :</a>
+					            <div class="description">{{ $kondisi->arahAngin }}</div>
+					        </div>
+					    </div>
+					</div>
+			    </div>
+			    <div class="ui segment">
+			    	<div class="ui relaxed divided list">
+					    <div class="item">
+					        <i class="large soundcloud middle aligned icon blue"></i>
+					        <div class="content">
+					            <a class="header">Kecepatan Angin :</a>
+					            <div class="description">{{ $kondisi->kecAngin }}</div>
+					        </div>
+					    </div>
+					</div>
+			    </div>
+			    <div class="ui segment">
+			    	<div class="ui relaxed divided list">
+					    <div class="item">
+					        <i class="large arrow down middle aligned icon blue"></i>
+					        <div class="content">
+					            <a class="header">Tekanan Udara :</a>
+					            <div class="description">{{ $kondisi->tekUdara }}</div>
+					        </div>
+					    </div>
+					</div>
+			    </div>
 			    <div class="ui segment">
 			    	<div class="ui relaxed divided list">
 					    <div class="item">
