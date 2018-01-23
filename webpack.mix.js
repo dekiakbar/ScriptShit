94401cb5c5727091e966ec9033e2f3a5ecc9d5e8
deki <dekiakbar@linuxmail.org>
2018-01-24 06:36:53 +0700

    pembersihan

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,4 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .copy('node_modules/semantic-ui-css/semantic.min.css','public/css/semantic.min.css')
-   .copy('node_modules/semantic-ui-css/themes','public/css/themes')
-   .copy('node_modules/semantic-ui-css/semantic.min.js','public/js/semantic.min.js');
+   .sass('resources/assets/sass/app.scss', 'public/css');
