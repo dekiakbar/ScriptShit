3f7e3821b9431d7ebad80a8f6308c96ae0e94484
deki <dekiakbar@linuxmail.org>
2018-03-13 05:50:15 +0700

    simbol celsius dan kelembaban

--- resources/views/admin/tanaman/Itanaman.blade.php
+++ resources/views/admin/tanaman/Itanaman.blade.php
@@ -6,29 +6,31 @@
 		<table class="ui single line celled table">
 		  	<thead>
 		    	<tr>
-		      		<th>Nama Tanaman</th>
-		      		<th>Suhu</th>
-		      		<th>Kelembaban</th>
-		      		<th>PH</th>
-		      		<th>Curah Hujan</th>
-		      		<th>Opsi</th>
+		    		<th class="center aligned">No</th>
+		      		<th class="center aligned">Nama Tanaman</th>
+		      		<th class="center aligned">Suhu (&#8451;)</th>
+		      		<th class="center aligned">Kelembaban (%)</th>
+		      		<th class="center aligned">PH</th>
+		      		<th class="center aligned">Curah Hujan (mm)</th>
+		      		<th class="center aligned">Opsi</th>
 		    	</tr>
 		  	</thead>
 		  	<tbody>
 		    	@foreach($datas as $data)
 		    		<tr>
-		    			<td>{{ $data->nama_tanaman }}</td>
-		    			<td>{{ $data->suhu->first()->suhuS1 }}</td>
-		    			<td>{{ $data->lembab->first()->lembabS1 }}</td>
-		    			<td>{{ $data->ph->first()->phS1 }}</td>
-		    			<td>{{ $data->curahHujan->first()->curahS1 }}</td>
+		    			<td class="center aligned">{{ ++$no }}</td>
+		    			<td class="center aligned">{{ $data->nama_tanaman }}</td>
+		    			<td class="center aligned">{{ $data->suhu->first()->suhuS1 }}</td>
+		    			<td class="center aligned">{{ $data->lembab->first()->lembabS1 }}</td>
+		    			<td class="center aligned">{{ $data->ph->first()->phS1 }}</td>
+		    			<td class="center aligned">{{ $data->curahHujan->first()->curahS1 }}</td>
 		    			<td class="center aligned">
-		    				<a href="" class="ui mini button red animated fade inverted">
+		    				<a href="" class="ui mini button blue animated fade inverted">
 		    					<div class="visible content">
-		    						Hapus
+		    						Detail
 		    					</div>
 		    					<div class="hidden content">
-		    						<i class="trash icon"></i>
+		    						<i class="zoom icon"></i>
 		    					</div>
 		    				</a>
 		    				<a href="" class="ui mini button purple animated fade inverted">
@@ -37,6 +39,14 @@
 		    					</div>
 		    					<div class="hidden content">
 		    						<i class="edit icon"></i>
+		    					</div>
+		    				</a>
+		    				<a href="" class="ui mini button red animated fade inverted">
+		    					<div class="visible content">
+		    						Hapus
+		    					</div>
+		    					<div class="hidden content">
+		    						<i class="trash icon"></i>
 		    					</div>
 		    				</a>
 		    			</td>
