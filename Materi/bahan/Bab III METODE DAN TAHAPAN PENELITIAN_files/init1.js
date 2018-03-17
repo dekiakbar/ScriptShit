720bee24c0d13456e4c9884147a4230ca40292f4
deki <dekiakbar@linuxmail.org>
2018-03-17 14:24:49 +0700

    backup materi

---
+++ Materi/bahan/Bab III METODE DAN TAHAPAN PENELITIAN_files/init1.js
@@ -0,0 +1,19 @@
+//<script>
+bouncex.state = {"newvid":false,"iol":true,"tn":1515221274,"pvid":5,"rc":false,"mobile":false,"vip":"36.84.227.235","geo":{"country_code":"ID","country_code3":"IDN","country_name":"Indonesia","region":"30","city":"Bandung","postal_code":null,"latitude":-6.9039,"longitude":107.6186,"area_code":null,"dma_code":null,"metro_code":null,"continent_code":"AS","zip":null,"country":"ID","region_name":"Jawa Barat"}};
+bouncex.cookie = {"v":{"ever_logged_in":true,"active_archive_purchase":false,"pmp_subscriber":false,"doc_title":"Bab III METODE DAN TAHAPAN PENELITIAN","doc_short_title":"Bab III METODE DAN TAHAPAN PENELITIAN","doc_category":false,"doc_type":"Archive","ibx_test":false,"interest":false,"campaign_timer":false,"sub_reported":false},"sid":10,"fvt":1510030082,"vid":1515221131084329,"ao":0,"lp":"https%3A%2F%2Fwww.scribd.com%2Fdoc%2F54108506%2FBab-III-METODE-DAN-TAHAPAN-PENELITIAN","as":0,"vpv":5,"d":"d","r":"www.google.com","cvt":1515221131,"gcr":70,"m":0,"did":"8845355564215115918","lvt":1515221274,"dgsig":"f0b46eaf9bd1bb772188"};
+bouncex.brandStyles = false;
+bouncex.webfonts = false;
+bouncex.campaigns = false;
+bouncex.gbi.stacks = false;
+bouncex.debug = false;
+
+
+bouncex.tryCatch(function(){
+	if(bouncex.browser.msie){
+		bouncex.setTimeout2(function(){
+			bouncex.init();
+		}, 0);
+	} else {
+		bouncex.init();
+	}
+})();
