af5ca62e85614e3bc6fa20f5f2af0dc6ecb94aec
dekiakbar <dekiakbar@linuxmail.org>
2018-04-25 20:39:03 +0700

    bismillah

--- routes/web.php
+++ routes/web.php
@@ -11,57 +11,60 @@
 |
 */
 
-Route::get('/', function () {
-    return view('admin.auth.daftar');
-});
+Route::domain('skripsi.com')->group(function(){
 
-/*
-|--------------------------------------------------------------------------
-| Routing untuk debug aplikasi
-|--------------------------------------------------------------------------
-*/
-Route::get('/cuaca','APIcontroller@cuaca');
-Route::get('/visualisasi', 'APIcontroller@Visualisasitempat');
-Route::get('/geo', 'APIcontroller@Geolokasi');
+	Route::get('/', function () {
+	    return view('admin.auth.daftar');
+	});
 
-Route::get('/detail/{lokasi}','APIcontroller@semua');
+	/*
+	|--------------------------------------------------------------------------
+	| Routing untuk debug aplikasi
+	|--------------------------------------------------------------------------
+	*/
+	Route::get('/cuaca','APIcontroller@cuaca');
+	Route::get('/visualisasi', 'APIcontroller@Visualisasitempat');
+	Route::get('/geo', 'APIcontroller@Geolokasi');
 
-/*
-|--------------------------------------------------------------------------
-| Override Routing admin
-|--------------------------------------------------------------------------
-*/
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+	Route::get('/detail/{lokasi}','APIcontroller@semua');
 
-Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Auth\RegisterController@register');
+	/*
+	|--------------------------------------------------------------------------
+	| Override Routing admin
+	|--------------------------------------------------------------------------
+	*/
+	Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+	Route::post('login', 'Auth\LoginController@login');
+	Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+	Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+	Route::post('register', 'Auth\RegisterController@register');
 
-/*
-|--------------------------------------------------------------------------
-| Route dashboard admin
-|--------------------------------------------------------------------------
-*/
-Route::get('/home', 'HomeController@index')->name('home');
+	Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+	Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+	Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+	Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-/*
-|--------------------------------------------------------------------------
-| Route tanaman
-|--------------------------------------------------------------------------
-*/
-Route::group(['prefix' => 'admin',  'middleware' => 'auth'], function(){
-	
-	Route::get('tanaman/tambah','tanamanCon@tambah');
-	Route::post('tanaman','tanamanCon@simpan')->name('tanaman.simpan');
-	Route::get('tanaman','tanamanCon@tampil');
-	Route::get('/tanaman/{id}/edit','tanamanCon@edit')->name('tanaman.edit');
-	Route::patch('/tanaman/{id}','tanamanCon@perbaharui')->name('tanaman.perbaharui');
-	Route::delete('/tanaman/{id}','tanamanCon@hapus')->name('tanaman.hapus');
-	
+	/*
+	|--------------------------------------------------------------------------
+	| Route dashboard admin
+	|--------------------------------------------------------------------------
+	*/
+	Route::get('/admin', 'HomeController@index')->name('home');
+
+	/*
+	|--------------------------------------------------------------------------
+	| Route tanaman
+	|--------------------------------------------------------------------------
+	*/
+	Route::group(['prefix' => 'admin',  'middleware' => 'auth'], function(){
+		
+		Route::get('tanaman/tambah','tanamanCon@tambah');
+		Route::post('tanaman','tanamanCon@simpan')->name('tanaman.simpan');
+		Route::get('tanaman','tanamanCon@tampil');
+		Route::get('/tanaman/{id}/edit','tanamanCon@edit')->name('tanaman.edit');
+		Route::patch('/tanaman/{id}','tanamanCon@perbaharui')->name('tanaman.perbaharui');
+		Route::delete('/tanaman/{id}','tanamanCon@hapus')->name('tanaman.hapus');
+	});
+
 });
